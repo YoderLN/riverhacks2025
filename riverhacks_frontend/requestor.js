87b9736a331a3f0f8eea0a38742707ceefc9f620
Guess <contact@andrewjonhardt.com>
2025-04-26 20:25:04 -0500

    data json testfile.

--- riverhacks_frontend/requestor.js
+++ riverhacks_frontend/requestor.js
@@ -2,10 +2,10 @@ const getData = (data) => {
     console.log(data);
 }
 const getDataFailed = (f) => {
-    console.log(this);
+    console.log(f);
 }
 
-const url = ""; //"http://localhost:5173/test.json";
+const url = "http://localhost:5173/data.json";
 const req = new XMLHttpRequest();
 req.responseType = 'json';
 req.onload = () => { getData(req.response) };
