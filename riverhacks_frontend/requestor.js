84f7556fabced75dfceacc9e6b1dbdf3a81aee84
Guess <contact@andrewjonhardt.com>
2025-04-26 19:39:09 -0500

    adding requestor script to index.

--- riverhacks_frontend/requestor.js
+++ riverhacks_frontend/requestor.js
@@ -1,12 +1,11 @@
 const getData = (data) => {
     console.log(data);
-    //console.log(JSON.parse(d));
 }
 const getDataFailed = (f) => {
     console.log(this);
 }
 
-const url = "http://localhost:5173/test.json";
+const url = ""; //"http://localhost:5173/test.json";
 const req = new XMLHttpRequest();
 req.responseType = 'json';
 req.onload = () => { getData(req.response) };
